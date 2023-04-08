1dee5ec068b009010634064b7df3fe4c0f8e251a
= <100027553@mvla.net>
2023-04-08 11:49:04 -0700

    Added basis for image scanning in the website - can turn on video and grab the image. Need to add processing for the image

--- starter-files/src/util/interact.js
+++ starter-files/src/util/interact.js
@@ -8,6 +8,27 @@ export const connectWallet = async () => {
   
 };
 
+export const validateVoter = async () => {
+    const video = document.querySelector('video');
+    const constraints = { video: true };
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(stream => {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(err => {
+            console.error('Error accessing camera:', err);
+        });
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    const scanButton = document.querySelector('#verify-button'); // Need to add scan/verify button
+    scanButton.addEventListener('click', () => {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        /* Process image */
+    });
+}   
+
 export const getCurrentWalletConnected = async () => {
   
 };
