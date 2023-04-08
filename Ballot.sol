ea718097b79ee04cb8d3fa2295ef7df8e2313449
Tohar Markovich <80293744+markovit-mvla@users.noreply.github.com>
2023-04-08 10:13:56 -0700

    Add files via upload

---
+++ Ballot.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract Ballot {
+    struct Voter {
+        bool voted;
+        address delegate;
+        uint vote;
+    }
+
+    Voter voter;
+
+    mapping (address => bool) public citizenshipStatus;
+    mapping (address => uint) public votesFor;
+    mapping (address => uint) public votesToUse;
+
+    event Vote(address from, address to, uint vote);
+
+    constructor(bool voted, address delegate, uint vote) {
+        voter = Voter(voted, delegate, vote);
+    } 
+
+    function vote(address _to, uint _vote) public returns (bool success) {
+        require(citizenshipStatus[msg.sender] == true, "Must be a citizen.");
+        require(votesToUse[msg.sender] == 1, "Cannot vote for multiple candidates.");
+
+        votesFor[_to] += _vote;
+        votesToUse[msg.sender] -= _vote;
+
+        emit Voter(msg.sender, _to, _vote);
+    }
+}
