b8666a631660274598194978adfb38cbf809199f
= <100027553@mvla.net>
2023-04-08 17:41:48 -0700

    Completely switched framework

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from '../logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="http://www.dappuniversity.com/bootcamp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Blockchain Ballots
+          </a>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <a
+                  href="http://www.dappuniversity.com/bootcamp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={logo} className="App-logo" alt="logo" style={{ width: '50%', height: '50%' }} />
+                </a>
+                <h1>Los Altos Hacks Blockchain Ballots</h1>
+                <p>
+                  Revolutionize voting.
+                </p>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
