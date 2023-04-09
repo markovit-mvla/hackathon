28bfe1f82777bbf2f9f9cc4985c4e5771bfad5ce
Tohar Markovich <80293744+markovit-mvla@users.noreply.github.com>
2023-04-08 19:01:30 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,61 @@ import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+  onClick() {
+    const video = document.querySelector('video');
+    const constraints = { video: true };
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(stream => {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(err => {
+            console.error('Error accessing camera:', err);
+        });
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    const scanButton = document.querySelector('#verify-button'); // Need to add scan/verify button
+    scanButton.addEventListener('click', () => {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        /*
+        <script onload="onOpenCvReady()"></script>
+        function onOpenCvReady() {
+            const src = cv.imread(canvas);
+            const gray = new cv.Mat();
+            let cv = cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
+            const imgData = new ImageData(new Uint8ClampedArray(gray.data), gray.cols, gray.rows);
+            const pixels = imgData.data;
+            // const grayscalePixels = [];
+
+            const grayscalePixel2 = new Array(canvas.height);
+            for(let i = 0; i < grayscalePixel2.length; i++) {
+              grayscalePixel2[i] = new Array(canvas.width);
+            }
+            for(let i = 0; i < canvas.height; i++) {
+              for(let j = 0; j < canvas.width; i++) {
+                let index = 4 *j + canvas.width * i;
+                const r = pixels[index];
+                const g = pixels[index + 1];
+                const b = pixels[index + 2];
+                const grayscaleValue = (r + g + b) / 3;
+                grayscalePixel2[i][j] = grayscaleValue;
+              }
+            }
+
+
+            // for (let i = 0; i < pixels.length; i += 4) {
+            //     const r = pixels[i];
+            //     const g = pixels[i + 1];
+            //     const b = pixels[i + 2];
+            //     const grayscaleValue = (r + g + b) / 3;
+            //     grayscalePixels.push(grayscaleValue);
+            // }
+          
+        }*/
+    });
+  }
+  
   render() {
     return (
       
@@ -33,6 +88,12 @@ class App extends Component {
                   Revolutionize voting.
                 </p>
                 <button id = "beg">Begin the voting process</button>
+                <script>
+                  function handleClick() {
+                    this.onClick()
+                  }
+                  document.getElementById("voteBegin").addEventListener("click", handleClick);
+                </script>
                 <p>This is a test</p>
               </div>
             </main>
